56f94acf0d9d106bd37c000420e46c56649457a3
Thomas Winters <winterst85@gmail.com>
2018-08-15 20:26:29 -0500

    added bootstrap, jquery, firebase - completed html with bootstrap

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDfJ3fRv6FZZrKSKqK9VMbfPiRaqQJeBmM",
+    authDomain: "train-scheduler-a4476.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a4476.firebaseio.com",
+    projectId: "train-scheduler-a4476",
+    storageBucket: "",
+    messagingSenderId: "1087108895896"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database();
